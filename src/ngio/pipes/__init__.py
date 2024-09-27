1464db46d4496d014065123980d3ca07c0bfb142
lorenzocerrone <lorenzocerrone@users.noreply.github.com>
2024-09-27 13:51:39 +0000

    deploy: 391ad8a5d3c7d26e522f27d6eb2a6334cb6bf5a6

--- src/ngio/pipes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A module to handle data transforms for image data."""
-
-from ngio.pipes._slicer_transforms import NaiveSlicer, RoiSlicer
-from ngio.pipes.data_pipe import DataTransformPipe
-
-__all__ = ["DataTransformPipe", "NaiveSlicer", "RoiSlicer"]
